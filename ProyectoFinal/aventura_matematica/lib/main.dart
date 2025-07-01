0ae62cc6adc496067c0079b5d90939acce23eeca
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-07-01 13:00:31 -0500

    Conectando el proyecto a firebase para luego usar la base de datos, autenticación y almacenamiento.

--- ProyectoFinal/aventura_matematica/lib/main.dart
+++ ProyectoFinal/aventura_matematica/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
